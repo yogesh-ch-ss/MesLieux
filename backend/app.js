5f4b7fd7ea9bb60d73d960e0ce1d67da000e6e01
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-08-31 02:44:47 +0530

    Error handling

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const placesRoutes = require('./routes/places-routes');
+const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
+// Middleware to Places => /api/places/...
 app.use("/api/places/", placesRoutes); // => /api/places/...
+
+// This middleware will only execute on requests that have an error
+app.use((error, req, res, next) => {
+  // if the response is sent, we forward (next) the error
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  // if no response has been sent
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
+});
 
 app.listen(5000);
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -16,20 +16,42 @@ const DUMMY_PLACES = [
   },
 ];
 
+// Finding a place by PlaceID
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
+  // If we don't find a place - Error 404
+  if (!place) {
+    const error = new Error(
+      "Could not find a place for the provided place id."
+    );
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place }); // => { place } => { place: place }
 });
 
+// Finding a place by Creator/UserID
 router.get("/user/:uid", (req, res, next) => {
-    const userId = req.params.uid; // { uid: 'u1' }
-    const place = DUMMY_PLACES.find((p) => {
-      return p.creator === userId;
-    });
-    res.json({ place }); // => { place } => { place: place }
+  const userId = req.params.uid; // { uid: 'u1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
   });
+
+  // If we don't find a place - Error 404
+  if (!place) {
+    const error = new Error(
+      "Could not find a place for the provided user id."
+    );
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+});
 
 module.exports = router;
