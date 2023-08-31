84991d71a83d9a6b43ee4445d17e882514ae7eee
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-08-31 16:08:26 +0530

    Adding Controllers

---
+++ backend/controllers/places-controllers.js
@@ -0,0 +1,57 @@
+const HttpError = require("../models/http-error");
+
+// This controller file is used to define middleware functions for /api/places/...
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9882393,
+    },
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u1",
+  },
+];
+
+// Contoller to: Finding a place by PlaceID
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  // If we don't find a place - Error 404
+  if (!place) {
+    throw new HttpError(
+      "Could not find a place for the provided place id.",
+      404
+    );
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+};
+
+// Controller to: // Finding a place by Creator/UserID
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid; // { uid: 'u1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  // If we don't find a place - Error 404
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+};
+
+// Exporting the pointer to the function to prevent express from executing the function
+// Ex: getPlaceById =/= getPlaceById()
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
