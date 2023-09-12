56fc5e0174d7935e36a3ea8c83c8d73e79265871
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-09-12 19:05:22 +0530

    Sending Login Request

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -70,10 +70,35 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true);
+
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          // .ok is to check 200ish response code - response ok
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login(); // updates the login() in auth context
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong. Please try again.");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -92,7 +117,7 @@ const Auth = () => {
           // .ok is to check 200ish response code - response ok
           throw new Error(responseData.message);
         }
-        console.log(responseData);
+
         setIsLoading(false);
         auth.login(); // updates the login() in auth context
       } catch (err) {
