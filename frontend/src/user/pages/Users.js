33692cd06bd06efb0d30d5f678477a53843c25f7
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-09-12 19:53:18 +0530

    GET request for Users

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/UsersList";
-
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 // Users page displays all the users and their places.
-// Connected to UsersList component 
+// Connected to UsersList component
 // Users -> UsersList -> UserItem
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Yogesh",
-      image:
-        "https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Dog-512.png",
-      places: 3
-    }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  // useEffect runs a certain code only when certain dependencies change
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:5000/api/users"); // GET is the default method
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.users);
+        }
+
+        setLoadedUsers(responseData.users);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
