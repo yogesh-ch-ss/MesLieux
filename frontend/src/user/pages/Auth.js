42088945dfcb30e15c9d6aba9fb2b1b193c75d81
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-09-15 02:11:56 +0530

    ImageUpload component

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -105,8 +106,6 @@ const Auth = () => {
     }
   };
 
-
-
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
@@ -127,6 +126,7 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && <ImageUpload center id="image" />}
           <Input
             element="input"
             id="email"
