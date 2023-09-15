2462a0a659e10942c5539e5978bceec1ca10a39d
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-09-15 22:56:11 +0530

    Multer setup for image upload

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get("/", usersControllers.getUsers);
 // Signup
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(), // normalizeEmail() = TesT@test.com => test@test.com
