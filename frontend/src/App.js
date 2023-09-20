3eed1c88d8ffab9bbdc196bfb5af6b2f41bf3261
Yogesh Chandra Singh Samant <yogeshchsamant@gmail.com>
2023-09-20 17:39:59 +0530

    Auto-Login and Auto-Logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,35 +15,64 @@ import Auth from "./user/pages/Auth";
 
 import { AuthContext } from "./shared/context/auth-context";
 
-// AuthAuthContext wraps all the routes.
+let logoutTimer; // To keep a track of logout time
 
 const App = () => {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
+    setUserId(uid);
+
+    const loginTokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(loginTokenExpirationDate);
+
     // Storing the token on Local Storage in the browser
     localStorage.setItem(
       "userData",
-      JSON.stringify({ userId: uid, token: token })
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: loginTokenExpirationDate.toISOString(),
+      })
     );
-    setUserId(uid);
   }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setTokenExpirationDate(null);
+    setUserId(null);
+    localStorage.removeItem("userData"); // Removing token from LocalStorage while logout
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   // Used to check if LocalStorage contains token info
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-    localStorage.removeItem("userData"); // Removing token from LocalStorage while logout
-  }, []);
 
   // Different set of routes should be available when Loggedin and NOT Loggedin
   let routes;
